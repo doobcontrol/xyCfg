553c4d7e23d8cd712e06207f5a089c1d9fe9c63d
he bo <dooacontrol@protonmail.com>
2026-03-05 02:15:44 +0800

    Feat: Support a list of strings as a parameter value (V1.0.5)

--- xyCfg/xyCfg.cs
+++ xyCfg/xyCfg.cs
@@ -24,58 +24,98 @@ namespace xy.Cfg
             return getCfgNode(genNode);
         }
 
-        private string getPar(string dType, string pName)
+        private object? getPar(string dType, string pName)
         {
             JsonObject cfgNode = getCfgNode(dType);
-            return cfgNode[pName].ToString();
+            var pValue = cfgNode[pName];
+            if (pValue is JsonValue)
+            {
+                return cfgNode[pName].GetValue<string>();
+            }
+            else if (pValue is JsonArray)
+            {
+                List<string> list = new List<string>();
+                foreach (var item in (JsonArray)pValue)
+                {
+                    list.Add(item.ToString());
+                }
+                return list;
+            }
+            return null;
         }
-        private string getPar(string pName)
+        private object? getPar(string pName)
         {
             return getPar(genNode, pName);
         }
-        private void setPar(string dType, string pName, string pValue)
+        private void setPar(string dType, string pName, object pValue)
         {
             JsonObject cfgNode = getCfgNode(dType);
-            cfgNode[pName] = pValue;
+            if(pValue is string)
+            {
+                cfgNode[pName] = pValue.ToString();
+            }
+            else if(pValue is List<string>)
+            {
+                JsonArray jsonArray = new JsonArray();
+                foreach (var item in (List<string>)pValue)
+                {
+                    jsonArray.Add(item);
+                }
+                cfgNode[pName] = jsonArray;
+            }
+
             string newJsonString = JsonSerializer.Serialize(cfgNode.Parent);
             File.WriteAllText(cfgFile, newJsonString);
         }
-        private void setPar(string dType, Dictionary<string, string> pValues)
+        private void setPar(string dType, Dictionary<string, object> pValues)
         {
             JsonObject cfgNode = getCfgNode(dType);
             foreach (var kvp in pValues)
             {
-                cfgNode[kvp.Key] = kvp.Value;
+                if (kvp.Value is string)
+                {
+                    cfgNode[kvp.Key] = kvp.Value.ToString();
+                }
+                else if (kvp.Value is List<string>)
+                {
+                    JsonArray jsonArray = new JsonArray();
+                    foreach (var item in (List<string>)kvp.Value)
+                    {
+                        jsonArray.Add(item);
+                    }
+                    cfgNode[kvp.Key] = jsonArray;
+                }
             }
             string newJsonString = JsonSerializer.Serialize(cfgNode.Parent);
             File.WriteAllText(cfgFile, newJsonString);
         }
-        private void setPar(string pName, string pValue)
+        private void setPar(string pName, object pValue)
         {
             setPar(genNode, pName, pValue);
         }
-        private void setPar(Dictionary<string, string> pValues)
+        private void setPar(Dictionary<string, object> pValues)
         {
             setPar(genNode, pValues);
         }
+
 
         private static string cfgFile = "xyCfg.json";
         private const string genNode = "genNode";
 
         static private xyCfg instance;
-        static public void init(Dictionary<string,string> initCfgList, string? cFile = null)
+        static public void init(Dictionary<string, object> initCfgList, string? cFile = null)
         {
             if (cFile != null)
             {
                 cfgFile = cFile;
             }
-            Dictionary<string, Dictionary<string, string>> initCfg
-                = new Dictionary<string, Dictionary<string, string>>();
+            Dictionary<string, Dictionary<string, object>> initCfg
+                = new Dictionary<string, Dictionary<string, object>>();
             initCfg.Add(genNode, initCfgList);
             init(initCfg);
         }
         static public void init(
-            Dictionary<string, Dictionary<string, string>> initCfg, string? cFile = null)
+            Dictionary<string, Dictionary<string, object>> initCfg, string? cFile = null)
         {
             if(cFile != null)
             {
@@ -90,7 +130,19 @@ namespace xy.Cfg
                     keyValuePairs = new JsonObject();
                     foreach (var item in kvp.Value)
                     {
-                        keyValuePairs[item.Key] = item.Value;
+                        if (item.Value is string)
+                        {
+                            keyValuePairs[item.Key] = item.Value.ToString();
+                        }
+                        else if (item.Value is List<string>)
+                        {
+                            JsonArray jsonArray = new JsonArray();
+                            foreach (var v in (List<string>)item.Value)
+                            {
+                                jsonArray.Add(v);
+                            }
+                            keyValuePairs[item.Key] = jsonArray;
+                        }
                     }
                     JsonObj.Add(kvp.Key, keyValuePairs);
                 }
@@ -101,27 +153,27 @@ namespace xy.Cfg
             instance = new xyCfg();
         }
 
-        static public string get(string dType, string pName)
+        static public object? get(string dType, string pName)
         {
             return instance.getPar(dType, pName);
         }
-        static public string get(string pName)
+        static public object? get(string pName)
         {
             return instance.getPar(genNode, pName);
         }
-        static public void set(string dType, string pName, string pValue)
+        static public void set(string dType, string pName, object pValue)
         {
             instance.setPar(dType, pName, pValue);
         }
-        static public void set(string pName, string pValue)
+        static public void set(string pName, object pValue)
         {
             instance.setPar(genNode, pName, pValue);
         }
-        static public void set(string dType, Dictionary<string, string> pValues)
+        static public void set(string dType, Dictionary<string, object> pValues)
         {
             instance.setPar(dType, pValues);
         }
-        static public void set(Dictionary<string, string> pValues)
+        static public void set(Dictionary<string, object> pValues)
         {
             instance.setPar(genNode, pValues);
         }
